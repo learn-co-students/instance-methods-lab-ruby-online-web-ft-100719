083e87ca9a07a3137834f941cca35b03e0e89767
Prince Charles <princecharles35@yahoo.com>
2019-10-22 19:42:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class dog
+ def bark
+    puts "Woof!"
+  end
+  def sit 
+    puts "the dog is sitting"
+end
+ 
+fido = Dog.new
+fido.bark
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+  def talk 
+    puts "hello world!".
+     def walk
+       puts "the person is walking".
+     end
+  end
+end   
+
+prince = Person.new
+person.talk
